170976dfa1ebe9fce93912622d50728be19a94b6
PTAEJOOO <qkrxown5985@gmail.com>
2025-01-05 02:20:36 +0900

    script

---
+++ scripts/physionet/S_Mamba_physionet.sh
@@ -0,0 +1,22 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=S_Mamba
+# d state 2
+python -u run_imts.py \
+  --is_training 1 \
+  --model_id physionet2012_36_12 \
+  --model $model_name \
+  --data physionet2012 \
+  --features M \
+  --seq_len 36 \
+  --pred_len 12 \
+  --e_layers 2 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --d_model 256 \
+  --d_state 2 \
+  --learning_rate 0.00005 \
+  --d_ff 256 \
+  --itr 1
