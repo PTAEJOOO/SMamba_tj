e5c77536c1628a9e10a275436fe93728ef62d6f7
PTAEJOOO <qkrxown5985@gmail.com>
2025-05-28 16:26:10 +0900

    aa

--- scripts/multivariate_forecasting/fintech/S_Mamba_fintech.sh
+++ scripts/multivariate_forecasting/fintech/S_Mamba_fintech.sh
@@ -8,7 +8,79 @@ python run.py \
     --data_path bitcoin_ethereum_transactions.csv \
     --data crypto \
     --model_id CryptoTx_96_96 \
-    --model S_Mamba \
+    --model $model_name \
+    --features M \
+    --target transactions_bitcoin \
+    --seq_len 96 \
+    --pred_len 12 \
+    --enc_in 2 \
+    --dec_in 2 \
+    --c_out 2 \
+    --des 'Exp' \
+    --d_model 256 \
+    --d_state 2\
+    --d_ff 256 \
+    --freq d \
+    --batch_size 32 \
+    --learning_rate 1e-4 \
+    --patience 5 \
+    --gpu 0
+
+python run.py \
+    --is_training 1 \
+    --root_path ./dataset/CryptoTx/ \
+    --data_path bitcoin_ethereum_transactions.csv \
+    --data crypto \
+    --model_id CryptoTx_96_96 \
+    --model $model_name \
+    --features M \
+    --target transactions_bitcoin \
+    --seq_len 96 \
+    --pred_len 24 \
+    --enc_in 2 \
+    --dec_in 2 \
+    --c_out 2 \
+    --des 'Exp' \
+    --d_model 256 \
+    --d_state 2\
+    --d_ff 256 \
+    --freq d \
+    --batch_size 32 \
+    --learning_rate 1e-4 \
+    --patience 5 \
+    --gpu 0
+
+python run.py \
+    --is_training 1 \
+    --root_path ./dataset/CryptoTx/ \
+    --data_path bitcoin_ethereum_transactions.csv \
+    --data crypto \
+    --model_id CryptoTx_96_96 \
+    --model $model_name \
+    --features M \
+    --target transactions_bitcoin \
+    --seq_len 96 \
+    --pred_len 48 \
+    --enc_in 2 \
+    --dec_in 2 \
+    --c_out 2 \
+    --des 'Exp' \
+    --d_model 256 \
+    --d_state 2\
+    --d_ff 256 \
+    --freq d \
+    --batch_size 32 \
+    --learning_rate 1e-4 \
+    --patience 5 \
+    --gpu 0
+
+python run.py \
+    --is_training 1 \
+    --root_path ./dataset/CryptoTx/ \
+    --data_path bitcoin_ethereum_transactions.csv \
+    --data crypto \
+    --model_id CryptoTx_96_96 \
+    --model $model_name \
     --features M \
     --target transactions_bitcoin \
     --seq_len 96 \
@@ -25,4 +97,3 @@ python run.py \
     --learning_rate 1e-4 \
     --patience 5 \
     --gpu 0
-
