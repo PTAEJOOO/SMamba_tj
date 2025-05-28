2b4f0096cfbf1bacb12e57aa684752eeafe3fd3d
PTAEJOOO <qkrxown5985@gmail.com>
2025-05-28 15:16:54 +0900

    aa

--- scripts/multivariate_forecasting/fintech/S_Mamba_fintech.sh
+++ scripts/multivariate_forecasting/fintech/S_Mamba_fintech.sh
@@ -16,30 +16,13 @@ python run.py \
     --enc_in 2 \
     --dec_in 2 \
     --c_out 2 \
+    --des 'Exp' \
+    --d_model 256 \
+    --d_state 2\
+    --d_ff 256 \
     --freq d \
     --batch_size 32 \
     --learning_rate 1e-4 \
+    --patience 5 \
     --gpu 0
-
-# python -u run.py \
-#   --is_training 1 \
-#   --root_path ./datasets/CryptoTx/ \
-#   --data_path ETTh1.csv \
-#   --model_id ETTh1_96_96 \
-#   --model $model_name \
-#   --data ETTh1 \
-#   --features M \
-#   --freq d \
-#   --seq_len 96 \
-#   --pred_len 96 \
-#   --e_layers 2 \
-#   --enc_in 7 \
-#   --dec_in 7 \
-#   --c_out 7 \
-#   --des 'Exp' \
-#   --d_model 256 \
-#   --d_state 2\
-#   --d_ff 256 \
-#   --itr 1 \
-#   --learning_rate 0.00007
 
